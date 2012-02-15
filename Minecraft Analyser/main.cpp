ae9549de598ce059ea3c523e6aa1a2814795789d
Yann Blaudin de Thé <ydethe@johnbook.(none)>
2012-02-15 22:03:05 +0100

    Initial Commit

---
+++ Minecraft Analyser/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Minecraft Analyser
+//
+//  Created by Yann Blaudin de Thé on 15/02/12.
+//  Copyright (c) 2012 EADS Astrium ST - P2010 - MAE. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
